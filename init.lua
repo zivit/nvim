12d6cfff6fa9387ae7345e60647a491fd1addaf4
zivit <zinc.vitaliy@gmail.com>
2022-08-05 00:52:21 +0300

    Added keymap for barbar

--- init.lua
+++ init.lua
@@ -9,4 +9,14 @@ vim.o.smartindent = true
 vim.o.expandtab = true
 
 -- Plugins settings
+require "plugins"
 vim.cmd("colorscheme onedarkpro")
+
+-- Keymap
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<Space>bc', '<Cmd>BufferClose<CR>', opts)
+map('n', '<Space>bp', '<Cmd>BufferPin<CR>', opts)
+map('n', '<Space>bk', '<Cmd>BufferPick<CR>', opts)
+
