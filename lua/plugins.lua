2119605c0608b157ba6bd7f93bcbe800646c4504
zivit <zinc.vitaliy@gmail.com>
2022-08-06 02:15:15 +0300

    Added comment plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,12 +7,13 @@ local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
+  print "Cloning packer..."
 end
 
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
@@ -81,6 +82,18 @@ return require('packer').startup(function(use)
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+            require("todo-comments").setup {}
+    end}      
+
+    use {'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
     
     if packer_bootstrap then
         require('packer').sync()
