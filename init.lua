f30db1639e1946e30cb56405926fdd06fd5c3306
zivit <zinc.vitaliy@gmail.com>
2022-08-06 04:20:29 +0300

    Updated cmp

--- init.lua
+++ init.lua
@@ -225,3 +225,24 @@ cmp.setup {
   },
 }
 
+require("cmp").setup({
+  window = {
+    completion = {
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = -3,
+      side_padding = 0,
+    },
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+      local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      kind.kind = " " .. strings[1] .. " "
+      --kind.menu = "    (" .. strings[2] .. ")"
+
+      return kind
+    end,
+  },
+})
+
