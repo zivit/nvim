566df2a6576241e6337de1f473928d141ca9f433
Vitaliy Zinchenko <vitaliy.zinchenko@utasco.com>
2022-08-31 12:43:49 +0300

    Added patterns for trim

--- lua/setup/trim.lua
+++ lua/setup/trim.lua
@@ -1,3 +1,9 @@
 require("trim").setup {
     disable = {"markdown"},
+    patterns = {
+        [[%s/\s\+$//e]],           -- remove unwanted spaces
+        -- [[%s/\($\n\s*\)\+\%$//]],  -- trim last line
+        [[%s/\%^\n\+//]],          -- trim first line
+        [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
+    },
 }
