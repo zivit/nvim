8564d07492de4c48cd821d339a6f6814e3f8f5ab
zivit <zinc.vitaliy@gmail.com>
2022-12-23 04:40:05 +0200

    Updated telescope

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -15,12 +15,15 @@ local keymap = vim.keymap.set
 local tb = require('telescope.builtin')
 local opts = { noremap = true, silent = true }
 
-keymap('v', '<space>fg', function()
+keymap('v', '<space>F', function()
 	local text = vim.getVisualSelection()
 	tb.live_grep({ default_text = text })
 end, opts)
 
 require('telescope').setup {
+    defaults = {
+        layout_strategy = "vertical",
+    },
     pickers = {
         find_files = {
             find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" }
