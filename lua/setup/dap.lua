919f13f5a210e583fc6d3d4bbc7d327f60e64960
zivit <zinc.vitaliy@gmail.com>
2022-12-28 00:35:23 +0200

    Updated dap plugin

--- lua/setup/dap.lua
+++ lua/setup/dap.lua
@@ -1,41 +1,51 @@
 local dap = require('dap')
 
-dap.adapters.lldb = {
-    type = 'executable',
-    command = '/opt/homebrew/opt/llvm/bin/lldb-vscode', -- adjust as needed, must be absolute path
-    name = 'lldb'
+if (vim.fn.has("osx") == 1) then
+    dap.adapters.lldb = {
+        type = 'executable',
+        command = '/opt/homebrew/opt/llvm/bin/lldb-vscode', -- adjust as needed, must be absolute path
+        name = 'lldb'
+    }
+elseif (vim.fn.has("linux") == 1) then
+    dap.adapters.lldb = {
+        type = 'executable',
+        command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+        name = 'lldb'
+    }
+end
+
+dap.configurations.cpp = {
+    {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
+    },
 }
---
--- dap.configurations.cpp = {
---     {
---         name = 'Launch',
---         type = 'lldb',
---         request = 'launch',
---         program = function()
---           return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
---         end,
---         cwd = '${workspaceFolder}',
---         stopOnEntry = false,
---         args = {},
---
---         -- 💀
---         -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
---         --
---         --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
---         --
---         -- Otherwise you might get the following error:
---         --
---         --    Error on launch: Failed to attach to the target process
---         --
---         -- But you should be aware of the implications:
---         -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
---         -- runInTerminal = false,
---     },
--- }
 --
 -- If you want to use this for Rust and C, add something like this:
 --
--- dap.configurations.c = dap.configurations.cpp
+dap.configurations.c = dap.configurations.cpp
+
+require('dap.ext.vscode').load_launchjs(nil, { cpp = {'c', 'cpp'} })
 
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
