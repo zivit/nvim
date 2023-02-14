c87ede16f4c357848660466cc7cc68aa3ee38af6
Vitaliy Zinchenko <vitaliy.zinchenko@utasco.com>
2023-02-14 12:34:42 +0200

    Updated autolist plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -134,7 +134,31 @@ return require('packer').startup { function(use)
     use { "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons",
         config = function() require("trouble").setup() end }
     use { "folke/which-key.nvim", config = function() require("which-key").setup {} end }
-    use { "gaoDean/autolist.nvim", config = function() require("autolist").setup() end }
+    use { "gaoDean/autolist.nvim",
+        ft = {
+            "markdown",
+            "text",
+            "tex",
+            "plaintex",
+        }, config = function()
+            local autolist = require("autolist")
+            autolist.setup()
+            autolist.create_mapping_hook("i", "<CR>", autolist.new)
+            autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
+            autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
+            autolist.create_mapping_hook("n", "o", autolist.new)
+            autolist.create_mapping_hook("n", "O", autolist.new_before)
+            autolist.create_mapping_hook("n", ">>", autolist.indent)
+            autolist.create_mapping_hook("n", "<<", autolist.indent)
+            autolist.create_mapping_hook("n", "<C-r>", autolist.force_recalculate)
+            autolist.create_mapping_hook("n", "<leader>x", autolist.invert_entry, "")
+            vim.api.nvim_create_autocmd("TextChanged", {
+                pattern = "*",
+                callback = function()
+                    vim.cmd.normal({ autolist.force_recalculate(nil, nil), bang = false })
+                end
+            })
+        end }
     use { "ggandor/lightspeed.nvim" }
     use { "glepnir/lspsaga.nvim", branch = "main", config = function() require("lspsaga").setup() end }
     use { "hrsh7th/cmp-buffer" }
